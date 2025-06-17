035f5e6356a1e6b5f11f3b893f2cb6bb007f0cfe
Carlos Doblas <carlos.doblas@nter.es>
2025-06-17 18:20:59 +0200

    feat: error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import gameRoutes from './routes/game'
-// import joinRoute from './routes/join'
 import sseRoute from './routes/sse'
+import {HttpError} from "./errors/httpError";
 
 const buildApp = async () => {
     const app = Fastify({logger: true});
@@ -10,6 +10,16 @@ const buildApp = async () => {
     await app.register(cors);
     await app.register(gameRoutes, {prefix: appPrefix});
     await app.register(sseRoute, {prefix: appPrefix});
+
+    app.setErrorHandler((error, request, reply) => {
+        if (error instanceof HttpError) {
+            reply.status(error.statusCode).send({ error: error.message });
+        } else {
+            reply.status(500).send({ error: 'Internal Server Error' });
+            console.error(error);
+        }
+    });
+
 
     app.get('/health', async () => {
         return {status: 'ok'};
---
+++ src/errors/httpError.ts
@@ -0,0 +1,10 @@
+// src/errors/HttpError.ts
+export class HttpError extends Error {
+    statusCode: number;
+
+    constructor(statusCode: number, message: string) {
+        super(message);
+        this.statusCode = statusCode;
+        Object.setPrototypeOf(this, HttpError.prototype);
+    }
+}
