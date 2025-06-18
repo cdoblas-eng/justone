25a16a64b0b349df26b899b414f8e28c3daa1cfe
Carlos Doblas <carlos.doblas@nter.es>
2025-06-18 13:43:16 +0200

    refactor: improved game errors checks

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -2,20 +2,50 @@ import {createNewGame, Game, GameStatus} from "../models/game";
 import {createPlayer} from "../models/player";
 import fs from 'fs'
 import path from 'path'
-import {sendMsg, sendMsgToAll} from "../utils/sse";
+import {sendMsg, sendMsgToAll} from "./sseService";
 import {HttpError} from "../errors/httpError";
 
 const filePath = path.join(__dirname, '../../assets/animales.csv')
 export const games: Record<string, Game> = {};
 export const gamePossibleWords: Record<string, string[]> = {};
 
+export function getGame(gameId: string) {
+    if (!games[gameId.trim()]) {
+        throw new HttpError(400, 'Invalid game ID or game doesnt exist.');
+    }
+    return games[gameId]
+}
+
+export function checkPlayerName(playerName: string) {
+    if (playerName.trim().length === 0) {
+        throw new HttpError(400, 'Invalid username name.');
+    }
+}
+
+export function checkPlayerNumber(game: Game) {
+    if (game.players.length < 2){
+        throw new HttpError(409, "There are no players playing in the game");
+    }
+}
+
+export function checkGameStatus(game: Game, requiredStatus: GameStatus) {
+    if (game.status !== requiredStatus) {
+        throw new HttpError(409, `Game status is not: ${requiredStatus}`);
+    }
+}
+
 export async function createGame(playerName: string): Promise<Game> {
+    checkPlayerName(playerName);
     const newGame: Game = createNewGame(playerName);
     games[newGame.id] = newGame;
     return newGame;
 }
 
 export async function joinGame(gameId: string, playerName: string) {
+    const game = getGame(gameId);
+    checkPlayerName(playerName);
+    checkGameStatus(game, GameStatus.WAITING_FOR_PLAYERS)
+
     const createdPlayer = createPlayer(playerName)
     games[gameId].players.push(createdPlayer);
     sendMsgToAll(gameId, {msg: playerName});
@@ -23,61 +53,45 @@ export async function joinGame(gameId: string, playerName: string) {
 }
 
 export async function startGame(gameId: string) {
-    if (canBePlayable(gameId)) {
-        const game = games[gameId];
-        sendMsgToAll(gameId, {msg: "THE GAME STARTED"});
-        games[gameId].activePlayerIndex = getRandomUniqueIndices(1, game.players.length)[0];
-        nextRound(gameId)
-    }
-}
+    const game = getGame(gameId);
+    checkGameStatus(game, GameStatus.WAITING_FOR_PLAYERS)
+    checkPlayerNumber(game);
 
-export async function stopGame(gameId: string) {
-    if (games[gameId]) {
-
-        games[gameId].players.forEach(player => {
-            sendMsg(player, {msg: "THE GAME FINISHED"});
-        })
-        delete games[gameId];
-    }
+    sendMsgToAll(gameId, {msg: "THE GAME STARTED"});
+    games[gameId].activePlayerIndex = getRandomUniqueIndices(1, game.players.length)[0];
+    await nextRound(gameId)
 }
 
 export async function nextRound(gameId: string) {
-    if (canBePlayable(gameId, GameStatus.WAITING_FOR_PLAYERS)) {
-        const game = games[gameId];
-        const nextGamePlayerIndex = (games[gameId].activePlayerIndex + 1) % game.players.length;
-        games[gameId].activePlayerIndex = nextGamePlayerIndex
-        game.clues = []
-        let player = game.players.at(nextGamePlayerIndex);
-        if (player) {
-            generatePossibleWords(gameId)
+    const game = getGame(gameId);
+    checkGameStatus(game, GameStatus.WAITING_FOR_PLAYERS)
+    checkPlayerNumber(game);
 
-            game.players.filter(p => p.id !== player.id).forEach(value => sendMsg(value, {msg: gamePossibleWords[gameId]}));
-            sendMsg(player, {msg: 'Select a number from 1 to 5'});
-            game.status = GameStatus.WAITING_FOR_NUMBER;
-        }
-    }
-}
+    const nextGamePlayerIndex = (games[gameId].activePlayerIndex + 1) % game.players.length;
+    games[gameId].activePlayerIndex = nextGamePlayerIndex
+    game.clues = []
+    let player = game.players.at(nextGamePlayerIndex);
+    if (player) {
+        generatePossibleWords(gameId)
 
-export function canBePlayable(gameId: string, gameStatus: GameStatus = GameStatus.WAITING_FOR_PLAYERS): boolean {
-    const game = games[gameId];
-    if (game && game.status !== gameStatus) {
-        throw new HttpError(409, `Game status is not: ${gameStatus}`);
+        game.players.filter(p => p.id !== player.id).forEach(value => sendMsg(value, {msg: gamePossibleWords[gameId]}));
+        sendMsg(player, {msg: 'Select a number from 1 to 5'});
+        game.status = GameStatus.WAITING_FOR_NUMBER;
     }
-    if (game.players.length < 2){
-        throw new HttpError(409, "There are no players playing in the game");
-    }
-    return true;
+
 }
 
 export async function setWord(gameId: string, number: number) {
-    const game = games[gameId];
-    if (canBePlayable(gameId, GameStatus.WAITING_FOR_NUMBER)) {
-        game.currentWord = gamePossibleWords[gameId][number];
-        const player = game.players[game.activePlayerIndex];
-        game.players.filter(p => p.id !== player.id).forEach(value => sendMsg(value, {msg: game.currentWord}));
-        game.status = GameStatus.WAITING_FOR_CLUES;
-        sendMsg(player, {msg: 'A la espera de pistas...'});
-    }
+    const game = getGame(gameId);
+    checkGameStatus(game, GameStatus.WAITING_FOR_NUMBER);
+    checkPlayerNumber(game);
+
+    game.currentWord = gamePossibleWords[gameId][number];
+    const player = game.players[game.activePlayerIndex];
+    game.players.filter(p => p.id !== player.id).forEach(value => sendMsg(value, {msg: game.currentWord}));
+    game.status = GameStatus.WAITING_FOR_CLUES;
+    sendMsg(player, {msg: 'A la espera de pistas...'});
+
 }
 
 function getRandomWordsFromCSV(filePath: string, count: number = 5): string[] {
@@ -113,5 +127,15 @@ function getRandomUniqueIndices(n: number, max: number): number[] {
 function generatePossibleWords(gameId:string){
     gamePossibleWords[gameId] = getRandomWordsFromCSV(filePath);
 }
+
+// export async function stopGame(gameId: string) {
+//     if (games[gameId]) {
+//
+//         games[gameId].players.forEach(player => {
+//             sendMsg(player, {msg: "THE GAME FINISHED"});
+//         })
+//         delete games[gameId];
+//     }
+// }
 
 
