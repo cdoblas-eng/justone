b0eaf8abb7d60af4b02d0e3ffdee5e4336b3966f
Carlos Doblas <carlos.doblas@nter.es>
2025-06-18 17:07:04 +0200

    refactor: combine index.ts and app.ts in server.ts

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import gameRoutes from './routes/game'
 import sseRoute from './routes/sse'
 import {HttpError} from "./errors/httpError";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const buildApp = async () => {
     const app = Fastify({logger: true});
     const appPrefix = process.env.APP_NAME || '';
@@ -28,4 +32,16 @@ const buildApp = async () => {
     return app;
 };
 
-export default buildApp;
+const start = async () => {
+    const app = await buildApp();
+    try {
+        await app.listen({port: Number(process.env.PORT) || 3000});
+        console.log("Server is running on port %d", process.env.PORT);
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
+
